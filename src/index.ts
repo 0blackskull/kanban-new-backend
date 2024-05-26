fd20eded52a8ca65b68bdb69130da28ccea0f1ab
0blackskull <admin@admins-MacBook-Air.local>
2024-05-26 18:17:21 +0530

    wip

--- src/index.ts
+++ src/index.ts
@@ -36,19 +36,22 @@ async function start() {
 
   app.use(express.json());
 
-  // app.use((req, res, next) => {
-  //   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173"); // Allow requests from frontend
-  //   res.setHeader(
-  //     "Access-Control-Allow-Methods",
-  //     "GET, POST, PUT, DELETE, OPTIONS"
-  //   );
-  //   res.setHeader(
-  //     "Access-Control-Allow-Headers",
-  //     "Content-Type, Authorization"
-  //   );
-  //   res.setHeader("Access-Control-Allow-Credentials", "true"); // Allow cookies
-  //   next();
-  // });
+  app.use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://kanban-frontend-1.netlify.app"
+    ); // Allow requests from frontend
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS, PATCH"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", "true"); // Allow cookies
+    next();
+  });
 
   app.use(
     express.urlencoded({
